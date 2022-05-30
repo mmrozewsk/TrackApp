87113cb458bdf479e59bd8ad309c2e10b48072e7
vast111 <84926026+vast111@users.noreply.github.com>
2022-05-30 22:52:29 +0200

    Horizontal

---
+++ app/src/main/java/com/example/trackapp/SplashScreenActivity.kt
@@ -0,0 +1,40 @@
+package com.example.trackapp
+
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+
+//        val animation = AnimationUtils.loadAnimation(this, R.anim.car_anim)
+//        val car = findViewById<ImageView>(R.id.car)
+//        car.startAnimation(animation)
+//
+//        Handler().postDelayed({
+//            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+//            finish()
+//        }, 4000.toLong())
+
+        val car = findViewById<ImageView>(R.id.car)
+        val objectAnimator = ObjectAnimator.ofFloat(car, "translationX", 1500f)
+        objectAnimator.duration=3000
+        objectAnimator.start()
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }, 4000.toLong())
+
+    }
+}
